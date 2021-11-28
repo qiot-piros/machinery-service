f301bf16571b769e2392379da67a2226de0a9b33
Simon Masschelein <simon.masschelein@xt-i.com>
2021-11-28 15:16:33 +0100

    Updating logging

--- src/main/java/iot/qiot/piros/edge/validation/consumer/ValidationMessageConsumer.java
+++ src/main/java/iot/qiot/piros/edge/validation/consumer/ValidationMessageConsumer.java
@@ -69,7 +69,7 @@ public class ValidationMessageConsumer implements Runnable {
 
   private void initConsumer() {
     if (jmsContext != null) {
-      LOG.info("qiot.latest-product-line - Closing existing context");
+      LOG.info("qiot.validation.consumer - Closing existing context");
       jmsContext.close();
     }
     jmsContext = connectionFactory.createContext(Session.AUTO_ACKNOWLEDGE);
@@ -86,6 +86,7 @@ public class ValidationMessageConsumer implements Runnable {
         String messagePayload = message.getBody(String.class);
         ValidationResponse response =
             objectMapper.readValue(messagePayload, ValidationResponse.class);
+        LOG.info("qiot.validation.consumer - Received validation response {}", messagePayload);
         if (response.isValid()) {
           ValidationSuccessEvent event = new ValidationSuccessEvent();
           event.setProductLineId(response.getProductLineId());
