728fe712281c4b083d040c79b9541887949e29b4
Simon Masschelein <simon.masschelein@xt-i.com>
2021-11-28 17:44:13 +0100

    Cleanup enum

--- src/main/java/iot/qiot/piros/edge/production/model/ProductionStage.java
+++ src/main/java/iot/qiot/piros/edge/production/model/ProductionStage.java
@@ -1,24 +1,18 @@
 package iot.qiot.piros.edge.production.model;
 
 public enum ProductionStage {
-  WEAVING("weaving", "coloring"),
-  COLORING("coloring", "printing"),
-  PRINTING("printing", "packaging"),
-  PACKAGING("packaging", "");
+  WEAVING("weaving"),
+  COLORING("coloring"),
+  PRINTING("printing"),
+  PACKAGING("packaging");
 
   private final String stageName;
-  private final String nextStage;
 
-  ProductionStage(String stageName, String nextStage) {
+  ProductionStage(String stageName) {
     this.stageName = stageName;
-    this.nextStage = nextStage;
   }
 
   public String getStageName() {
     return this.stageName;
-  }
-
-  public String getNextStage() {
-    return this.nextStage;
   }
 }
